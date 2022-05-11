8410778e354a6b56f334dec5672234164911c886
adilsonlima <addy1997lima@gmail.com>
2022-05-11 16:45:03 -0100

    Atualizado em 11/05/2022

--- app/src/main/java/com/example/farmacia_v1/LoginActivity.java
+++ app/src/main/java/com/example/farmacia_v1/LoginActivity.java
@@ -18,7 +18,6 @@ public class LoginActivity extends AppCompatActivity {
 
     EditText user, pass;
     Button reg,log;
-    Spinner _spinner;
     DBHelper DB;
 
     @Override
@@ -30,9 +29,6 @@ public class LoginActivity extends AppCompatActivity {
         pass = findViewById(R.id.password);
         reg = findViewById(R.id.btn_registrar);
         log = findViewById(R.id.btn_login);
-        _spinner = findViewById(R.id.spinner);
-        ArrayAdapter <CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.usertype, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
-        _spinner.setAdapter(adapter);
         DB = new DBHelper(this);
 
         log.setOnClickListener(new View.OnClickListener() {
@@ -40,7 +36,6 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String user1 = user.getText().toString();
                 String pass1 = pass.getText().toString();
-                String item = _spinner.getSelectedItem().toString();
 
                 if (TextUtils.isEmpty(user1) || TextUtils.isEmpty(pass1))
                     Toast.makeText(LoginActivity.this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
@@ -53,17 +48,6 @@ public class LoginActivity extends AppCompatActivity {
                     }else{
                         Toast.makeText(LoginActivity.this, "Falha em Logar", Toast.LENGTH_SHORT).show();
                     }
-                }
-
-                /*ira verifica o tipo de usuario*/
-                if(user1.equals("admin") && pass1.equals("admin") && item.equals("admin")){
-                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);/*mudar o nome da classe home para admin*/
-                    startActivity(intent);
-                }else if (user1.equals("admin") && pass1.equals("admin") && item.equals("user")){
-                    Intent intent = new Intent(LoginActivity.this, UserActivity.class);
-                    startActivity(intent);
-                }else{
-                    Toast.makeText(getApplicationContext(), "ERRO", Toast.LENGTH_SHORT).show();
                 }
             }
         });
