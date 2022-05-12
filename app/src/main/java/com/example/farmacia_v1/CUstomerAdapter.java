02a495795e77d5d03997ca53208486a87cee7042
adilsonlima <addy1997lima@gmail.com>
2022-05-12 19:25:23 -0100

    Atualizado em 12/05/2022 19:25

--- app/src/main/java/com/example/farmacia_v1/CUstomerAdapter.java
+++ app/src/main/java/com/example/farmacia_v1/CUstomerAdapter.java
@@ -1,11 +1,13 @@
 package com.example.farmacia_v1;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,6 +18,7 @@ import java.util.ArrayList;
 public class CUstomerAdapter extends RecyclerView.Adapter<CUstomerAdapter.MyViewHolder> {
     private Context context;
     private ArrayList remedio_id, remedio_nome, remedio_quantidade, remedio_descricao;
+
 
     CUstomerAdapter(Context context, ArrayList remedio_id, ArrayList remedio_nome,ArrayList remedio_quantidade,ArrayList remedio_descricao){
         this.context = context;
@@ -34,15 +37,19 @@ public class CUstomerAdapter extends RecyclerView.Adapter<CUstomerAdapter.MyView
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, @SuppressLint("RecyclerView") final int position) {
         holder.remedio_id_txt.setText(String.valueOf(remedio_id.get(position)));
         holder.remedio_nome_txt.setText(String.valueOf(remedio_nome.get(position)));
         holder.remedio_quantidade_txt.setText(String.valueOf(remedio_quantidade.get(position)));
         holder.remedio_descricao_txt.setText(String.valueOf(remedio_descricao.get(position)));
-        holder.update.setOnClickListener(new View.OnClickListener() {
+        holder._mainLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(context, UpdateActivity.class);
+                intent.putExtra("remedio_id", String.valueOf(remedio_id.get(position)));
+                intent.putExtra("remedio_nome", String.valueOf(remedio_nome.get(position)));
+                intent.putExtra("remedio_quantidade", String.valueOf(remedio_quantidade.get(position)));
+                intent.putExtra("remedio_descricao", String.valueOf(remedio_descricao.get(position)));
                 context.startActivity(intent);
             }
         });
@@ -57,14 +64,14 @@ public class CUstomerAdapter extends RecyclerView.Adapter<CUstomerAdapter.MyView
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
         TextView remedio_id_txt, remedio_nome_txt, remedio_quantidade_txt, remedio_descricao_txt;
-        Button update;
+        LinearLayout _mainLayout;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             remedio_id_txt = itemView.findViewById(R.id.id_remedio);
             remedio_nome_txt = itemView.findViewById(R.id.nome_remedio);
             remedio_quantidade_txt = itemView.findViewById(R.id.quan_remedio);
             remedio_descricao_txt = itemView.findViewById(R.id.des_remedio);
-            update = itemView.findViewById(R.id.update_btn);
+            _mainLayout = itemView.findViewById(R.id.mainLayout);
         }
     }
 }
--- app/src/main/java/com/example/farmacia_v1/UpdateActivity.java
+++ app/src/main/java/com/example/farmacia_v1/UpdateActivity.java
@@ -3,12 +3,50 @@ package com.example.farmacia_v1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class UpdateActivity extends AppCompatActivity {
+
+    EditText nome_remedio_input, quant_remedio_input, desc_remedio_input;
+    Button btn_update;
+    String id, nome, quant, desc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+        nome_remedio_input = findViewById(R.id.input_nome_remedio2);
+        quant_remedio_input = findViewById(R.id.quantidade_remedio2);
+        desc_remedio_input = findViewById(R.id.descricao_remedio2);
+        btn_update = findViewById(R.id.update_btn);
+
+        btn_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        getAnsSetIntentData();
     }
-}
+        void getAnsSetIntentData(){
+            if(getIntent().hasExtra("remedio_id") && getIntent().hasExtra("remedio_nome") &&
+                    getIntent().hasExtra("remedio_quantidade") && getIntent().hasExtra("remedio_descricao")){
+                //Getting Data from Intent
+                id = getIntent().getStringExtra("remedio_id");
+                nome = getIntent().getStringExtra("remedio_nome");
+                quant = getIntent().getStringExtra("remedio_quantidade");
+                desc = getIntent().getStringExtra("remedio_descricao");
+
+                //Setting Intent Data
+                nome_remedio_input.setText(nome);
+                quant_remedio_input.setText(quant);
+                desc_remedio_input.setText(desc);
+            }else{
+                Toast.makeText(this, "No Data.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
