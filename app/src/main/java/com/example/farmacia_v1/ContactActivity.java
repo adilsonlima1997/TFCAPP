63790115377e1f0b2f2d392467bce95a36817042
adilsonlima <addy1997lima@gmail.com>
2022-05-18 11:27:42 -0100

    Atualizado em 18/05/2022 11:27

--- app/src/main/java/com/example/farmacia_v1/ContactActivity.java
+++ app/src/main/java/com/example/farmacia_v1/ContactActivity.java
@@ -1,26 +1,42 @@
 package com.example.farmacia_v1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class ContactActivity extends AppCompatActivity {
 
-    ImageView voltar;
+    ImageView voltar; Button _phoneCall;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
 
         voltar = findViewById(R.id.seta_voltar);
+        _phoneCall = findViewById(R.id.button2);
 
         voltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), InfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //função que lhe dá a possibilidade de fazer chamadas apartir do app
+        _phoneCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:5925806"));
                 startActivity(intent);
             }
         });
