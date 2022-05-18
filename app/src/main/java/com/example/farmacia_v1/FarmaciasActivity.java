0dd56b8c907605ce878651e48278cbc7a527e953
adilsonlima <addy1997lima@gmail.com>
2022-05-18 20:07:56 -0100

    Atualizado em 18/05/2022 20:08

---
+++ app/src/main/java/com/example/farmacia_v1/FarmaciasActivity.java
@@ -0,0 +1,14 @@
+package com.example.farmacia_v1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FarmaciasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmacias);
+    }
+}
