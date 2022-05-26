373833a1858b42dd34bdc48320d4980637025048
adilsonlima <addy1997lima@gmail.com>
2022-05-26 16:14:53 -0100

    Atualizado em 26/05/2022 16:14

--- app/src/main/java/com/example/farmacia_v1/ClinicasActivity.java
+++ app/src/main/java/com/example/farmacia_v1/ClinicasActivity.java
@@ -1,18 +1,28 @@
 package com.example.farmacia_v1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+import Base_dados.DBClinicas;
 
 public class ClinicasActivity extends AppCompatActivity {
 
     RecyclerView recyclerView_1;
     FloatingActionButton btn_add_1;
+    DBClinicas myDB;
+    ArrayList<String> id_clinica, nome_clinica, local_clinica, hora_clinica;
+    CustomerAdapterClinica customerAdapterClinica;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +39,32 @@ public class ClinicasActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        myDB = new DBClinicas(ClinicasActivity.this);
+        id_clinica = new ArrayList<>();
+        nome_clinica = new ArrayList<>();
+        local_clinica = new ArrayList<>();
+        hora_clinica = new ArrayList<>();
+        displayData();
+        customerAdapterClinica = new CustomerAdapterClinica(ClinicasActivity.this, id_clinica, nome_clinica, local_clinica,hora_clinica);
+        recyclerView_1.setAdapter(customerAdapterClinica);
+        recyclerView_1.setLayoutManager(new LinearLayoutManager(ClinicasActivity.this));
+
+
+    }
+
+    public void displayData() {
+        Cursor cursor = myDB.readAllData2();
+
+        if (cursor.getCount() == 0) {
+            Toast.makeText(this, "No data", Toast.LENGTH_SHORT).show();
+        } else {
+            while (cursor.moveToNext()) {
+                id_clinica.add(cursor.getString(0));
+                nome_clinica.add(cursor.getString(1));
+                local_clinica.add(cursor.getString(2));
+                hora_clinica.add(cursor.getString(3));
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/farmacia_v1/CustomerAdapterClinica.java
@@ -0,0 +1,59 @@
+package com.example.farmacia_v1;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CustomerAdapterClinica extends RecyclerView.Adapter<CustomerAdapterClinica.MyViewHolder>{
+
+    private Context context;
+    private ArrayList clinica_id, clinica_nome, clinica_localizacao, clinica_hora;
+
+    CustomerAdapterClinica(Context context,  ArrayList clinica_id,ArrayList clinica_nome, ArrayList clinica_localizacao, ArrayList clinica_hora){
+        this.context = context;
+        this.clinica_id = clinica_id;
+        this.clinica_nome = clinica_nome;
+        this.clinica_localizacao = clinica_localizacao;
+        this.clinica_hora = clinica_hora;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.recycler_view_clinica, parent, false);
+        return new CustomerAdapterClinica.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.clinica_id_txt.setText(String.valueOf(clinica_id.get(position)));
+        holder.clinica_nome_txt.setText(String.valueOf(clinica_nome.get(position)));
+        holder.clinica_localizacao_txt.setText(String.valueOf(clinica_localizacao.get(position)));
+        holder.clinica_hora_txt.setText(String.valueOf(clinica_hora.get(position)));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return clinica_id.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView clinica_id_txt, clinica_nome_txt, clinica_localizacao_txt, clinica_hora_txt;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            clinica_id_txt  = itemView.findViewById(R.id.id_CLINICA);
+            clinica_nome_txt  = itemView.findViewById(R.id.nome_CLINICA);
+            clinica_localizacao_txt  = itemView.findViewById(R.id.Localizacao_Clinica);
+            clinica_hora_txt  = itemView.findViewById(R.id.horas);
+        }
+    }
+}
