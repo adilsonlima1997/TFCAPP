a4c60eb3d756e7a6be1545ba178b55f8e479dda7
adilsonlima <addy1997lima@gmail.com>
2022-10-02 14:51:59 -0100

    Atualizado em 02/10/2022 14:50

--- app/src/main/java/com/example/farmacia_v1/ConsultaActivity.java
+++ app/src/main/java/com/example/farmacia_v1/ConsultaActivity.java
@@ -3,12 +3,14 @@ package com.example.farmacia_v1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import android.widget.Spinner;
@@ -20,8 +22,9 @@ import Base_dados.DBClinicas;
 
 
 public class ConsultaActivity extends AppCompatActivity {
-    ImageView voltar;
-    Button btn_enviar;
+    EditText email, _subject,_body;
+    Button btn_send;
+    ImageView back;
 
 
     @Override
@@ -29,25 +32,42 @@ public class ConsultaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consulta);
 
-        voltar = findViewById(R.id.imageView3);
-        btn_enviar = findViewById(R.id.enviar);
+        email = findViewById(R.id.emailAddressClinica);
+        _subject = findViewById(R.id.subjectClinica);
+        _body = findViewById(R.id.multEmailClinica);
+        btn_send = findViewById(R.id.sendClinica);
+        back = findViewById(R.id._backclinic);
+
+      btn_send.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              //verifica se os campos estão todos preenchidos de acordo com o que está a ser pedido
+              if (!email.getText().toString().isEmpty() && !_subject.getText().toString().isEmpty() && !_body.getText().toString().isEmpty()){
+                  Intent intent = new Intent(Intent.ACTION_SENDTO);
+                  intent.putExtra(Intent.EXTRA_EMAIL, new String[]{email.getText().toString()});
+                  intent.putExtra(Intent.EXTRA_SUBJECT, _subject.getText().toString());
+                  intent.putExtra(Intent.EXTRA_TEXT, _body.getText().toString());
+                  //---intent.setType("message/rfc822");----
+                  intent.setData(Uri.parse("mailto:"));
+                  //verificar se tem algum app no telemovel que pode executar a activity senão lança uma mensagem de erro
+                  if(intent.resolveActivity(getPackageManager()) != null){
+                      startActivity(intent);
+                  }else{
+                      Toast.makeText(ConsultaActivity.this, "Não aplicativo que suporta essa ação.", Toast.LENGTH_SHORT).show();
+                  }
+              }else{
+                  Toast.makeText(ConsultaActivity.this, "Por favor preencher os campos!", Toast.LENGTH_SHORT).show();
+              }
+          }
+      });
 
 
-
-        voltar.setOnClickListener(new View.OnClickListener() {
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                Intent intent = new Intent(getApplicationContext(), ClinicActivity.class);
                 startActivity(intent);
-            }
-        });
-
-        btn_enviar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), ConsultaActivity.class);
-                startActivity(intent);
-                Toast.makeText(ConsultaActivity.this, "Consulta Marcado", Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
 
